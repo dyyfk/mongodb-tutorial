8f217e9e5b11e77cfd3e8040c0939c3dcf0786ed
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-01-24 10:06:58 -0600

    add post and get

--- server/server.js
+++ server/server.js
@@ -1,38 +1,27 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo',{
-	text: {
-		type: String
-	},
-	completed: {
-		type: Boolean
-	},
-	completedAt: {
-		type: Number
-	}
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+	var todo = new Todo({
+		text: req.body.text,
+	});
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	});
 });
 
-var newTodo = new Todo({
-	text: 'Cook dinner'
+app.listen(3000,()=>{
+	console.log('Port listening to 3000');
 });
 
-newTodo.save().then((doc)=>{
-	console.log('Saved todo',doc);
-},(e)=>{
-	console.log('Unable to save todo' + e);
-});
 
-var otherTodo = new Todo({
-	text: 'take shower',
-	completed: false,
-	completedAt: 123
-});
-
-otherTodo.save().then((doc)=>{
-	console.log(JSON.stringify(doc, undefined,2));
-},(e)=>{
-	console.log('Unable to save todo' + e);
-});
