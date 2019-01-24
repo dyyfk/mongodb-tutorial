25f7fac1015ac103e495403e15748b002a1dc828
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-01-24 13:27:37 -0600

    add get/todo route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.post('/todos',(req,res)=>{
 	});
 });
 
+app.get('/todos',(req,res)=>{
+	Todo.find().then((todos)=>{
+		res.send({
+			todos
+		});
+	},(e)=>{
+		console.log(e);
+	});
+});
+
 app.listen(3000,()=>{
 	console.log('Port listening to 3000');
 });
