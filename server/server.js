898c60d49ce92f3b1ef3862e3d0795a9b251563d
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-02-05 10:12:05 -0600

    add post/ users route and token method

--- server/server.js
+++ server/server.js
@@ -70,6 +70,20 @@ app.delete('/todos/:id',(req,res)=>{
 	});
 });
 
+app.post('/users',(req,res)=>{
+	var body = _.pick(req.body, ['email','password']);
+	var user = new User(body);							 
+	
+	user.save().then(()=>{
+//		res.send(user);	
+        return user.generateAuthToken();
+	}).then((token)=>{
+        res.header('x-auth',token).send(user);
+    }).catch((e)=>{
+		res.status(400).send(e);					 
+	});
+});
+
 app.patch('/todos/:id',(req,res)=>{
 	var id = req.params.id;
 	var body = _.pick(req.body, ['text', 'completed']);
